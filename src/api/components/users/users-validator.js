44e9f7ed45600f010500f2399de2b5e5f9046e12
pizzaguy21 <stevenwijaya0405@gmail.com>
2024-04-11 23:20:13 +0700

    Tugas

---
+++ src/api/components/users/users-validator.js
@@ -0,0 +1,24 @@
+const joi = require('joi');
+
+module.exports = {
+  createUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+      password: joi.string().min(6).max(32).required().label('Password'),
+      confirm_password: joi
+        .string()
+        .min(6)
+        .max(32)
+        .required()
+        .label('Confirm Password'),
+    },
+  },
+
+  updateUser: {
+    body: {
+      name: joi.string().min(1).max(100).required().label('Name'),
+      email: joi.string().email().required().label('Email'),
+    },
+  },
+};
